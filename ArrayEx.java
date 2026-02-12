0d5216d14d815dfcffe1ccb384052dfb91437d01
KARTIK <137619812+kartikcreate@users.noreply.github.com>
2026-02-12 23:42:04 +0530

    Add files via upload

---
+++ ArrayEx.java
@@ -0,0 +1,20 @@
+public class ArrayEx {
+    public static void main(String[] args) {
+        int numbers[]= new int[4];
+        numbers[0]=1;
+                numbers[1]=1;
+        numbers[2]=1;
+        numbers[3]=1;
+        int realnumbers[]={1,2,3,4,5};
+for(int i=1;i<=3;i++)
+{
+System.out.print(numbers[i]);
+}
+System.out.println("");
+for(int j=0;j<=4;j++){
+    System.out.println(realnumbers[j]);
+}
+
+    }
+    
+}
